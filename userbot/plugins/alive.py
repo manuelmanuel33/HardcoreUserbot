edecba1bbc9db92840b7bbf7c5bfa531255a9017
manuelmanuel33 <66557381+manuelmanuel33@users.noreply.github.com>
2020-06-07 23:03:13 +0200

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -11,7 +11,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba"
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**`💦💦💦💦💦💦💦💦💦💦💦💦💦💦💦💦💦💦💦💦💦💦`**\n\n"
+    await alive.edit("**`💦💦SI AMORE STO BENISSIMO💦💦`**\n\n"
                      "**💦Telethon version:- 6.9.0**\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\n**💦Python: 3.7.3**\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\n"
                      "**💦Bot creato da :- @Expoosed\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\n**"
                      "**💦Database Stato: Databases funziona benissimo!**\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\n💞Sepmpre con te, boss!\n`"
